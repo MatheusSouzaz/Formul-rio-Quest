6ff683a468462ff842925bd1948ffa34748c8adc
MatheusSouzaz <matheusarasouza0515@gmail.com>
2024-04-07 10:54:11 -0300

    refatoração-js

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,80 +1,20 @@
-let campos = document.querySelectorAll(".campo")
+let camposFormulario = document.querySelectorAll(".campo")
+let botaoEnviar = document.querySelector(".btn-enviar")
 
+botaoEnviar.addEventListener('click',function(e){
+e.preventDefault()
 
-
-
-
-campos.forEach(input =>{
-    input.addEventListener('input', function(event){
-
-        let input = event.target
-
-        let tamanhoConteudo = event.target.value.length
-        
-
-        if(tamanhoConteudo > 0){
-
-            input.classList.add("preenchido")
-        }
-        else{
-            input.classList.remove("preenchido")
-        }
-    })
+camposFormulario.forEach((input)=>{
+    if(input.value){
+        input.classList.add('preenchido')
+        input.classList.remove('vazio')
+        input.nextElementSibling.classList.remove('mostrar')
+    }else{
+        input.classList.remove('preenchido')
+        input.classList.add('vazio')
+        input.nextElementSibling.classList.add('mostrar')
+    }
 })
-
-let btnEnviar = document.querySelector(".btn-enviar")
-
-btnEnviar.addEventListener("click", function(){
-
- const camposObrigatorios = document.querySelectorAll(".campo-vazio")
-
-
-
-
-    campos.forEach(input =>{
-
-        let conteudoInput = input.value
-
-        if(conteudoInput === ""){
-            input.classList.add("vazio")
-    
-        }
-        else{
-            input.classList.remove("vazio")
-            
-        }})
-let classesCampo1 = campos[0].classList
-let classesCampo2 = campos[1].classList
-let classesCampo3 = campos[2].classList
-let classesCampo4 = campos[3].classList
-
-
-if(classesCampo1[1] === "vazio"){
-    camposObrigatorios[0].classList.add("verdade")
-}else{
-    camposObrigatorios[0].classList.remove("verdade")
-}
-
-if(classesCampo2[1] === "vazio"){
-    camposObrigatorios[1].classList.add("verdade")
-}else{
-    camposObrigatorios[1].classList.remove("verdade")
-}
-if(classesCampo3[1] === "vazio"){
-    camposObrigatorios[2].classList.add("verdade")
-}else{
-    camposObrigatorios[2].classList.remove("verdade")
-}
-if(classesCampo4[1] === "vazio"){
-    camposObrigatorios[3].classList.add("verdade")
-}else{
-    camposObrigatorios[3].classList.remove("verdade")
-}
-
-
-
-
-
 })
 
 
